e727f228daa80697c6834f7ca74b1a2d243b182e
Tijn Kersjes <tijn@reaktor.fi>
2017-10-31 15:10:40 +0100

    Bugfix

--- server/githubService.js
+++ server/githubService.js
@@ -13,10 +13,10 @@ function apiCall(url, headers = {}) {
     }
   } else if (config.token ) {
     options.auth = {
-      username: config.token 
+      username: config.token
     }
   };
-  
+
   return axios.get(url, options);
 }
 
@@ -89,7 +89,7 @@ function getPullRequestReviews(pr) {
       return total + (review.state === 'APPROVED' ? 1 : 0)
     }, 0)
     pr.negativeReviews = reviews.data.reduce((total, review) => {
-      return total + (review.state === 'REQUEST_CHANGES' ? 1 : 0)
+      return total + (review.state === 'CHANGES_REQUESTED' ? 1 : 0)
     }, 0)
   });
 }
@@ -120,7 +120,7 @@ exports.loadPullRequests = function loadPullRequests() {
     const reviewsPromises = prs.map(pr => getPullRequestReviews(pr));
     return Promise.all(reviewsPromises).then(() => prs);
   })
-  .then(prs => prs.filter(pr => pr.positiveReviews > 0 && pr.negativeReviews === 0))
+  .then(prs => prs.filter(pr => pr.positiveReviews === 0 || pr.negativeReviews > 0))
   .then(prs => {
     const commentsPromises = prs.map(pr => getPullRequestComments(pr));
     return Promise.all(commentsPromises).then(() => prs);
@@ -132,21 +132,21 @@ exports.loadPullRequests = function loadPullRequests() {
   .then(prs => {
     const statusPromises = prs.map(pr => getPullRequestStatus(pr));
     return Promise.all(statusPromises).then(() => prs);
-  }).then(prs => {
-      prs.sort((p2, p1) => new Date(p2.updated).getTime() - new Date(p1.updated).getTime());
-      if (configManager.hasMergeRules()) {
-        prs.forEach(pr => {
-          if (config.mergeRule.neverRegexp && configManager.getNeverMergeRegexp().test(pr.title)) {
-            pr.unmergeable = true;
-          } else if (pr.positiveComments >= config.mergeRule.positive &&
-              pr.negativeComments <= config.mergeRule.negative) {
-            pr.mergeable = true;
-          } else if(moment(pr.created).isBefore(moment().subtract(7, "days"))) {
-            pr.unmergeable = true;
-          }
-        });
-      }
-      return prs;
-    });
+  })
+  .then(prs => {
+    prs.sort((p2, p1) => new Date(p2.updated).getTime() - new Date(p1.updated).getTime());
+    if (configManager.hasMergeRules()) {
+      prs.forEach(pr => {
+        if (config.mergeRule.neverRegexp && configManager.getNeverMergeRegexp().test(pr.title)) {
+          pr.unmergeable = true;
+        } else if (pr.positiveComments >= config.mergeRule.positive &&
+            pr.negativeComments <= config.mergeRule.negative) {
+          pr.mergeable = true;
+        } else if(moment(pr.created).isBefore(moment().subtract(7, "days"))) {
+          pr.unmergeable = true;
+        }
+      });
+    }
+    return prs;
   });
 };
